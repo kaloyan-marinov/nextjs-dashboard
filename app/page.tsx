2ae2083db3fb48402535c2574c09026b3d57e4cd
Kaloyan Marinov <happy-2025-04-06-14-33@protonmail.com>
2026-05-18 00:11:04 +0200

    make `app/page.tsx` render the image at `public/hero-mobile.png` according to this: It should be shown on mobile screens, and hidden on desktop

--- app/page.tsx
+++ app/page.tsx
@@ -38,12 +38,21 @@ export default function Page() {
           the class `hidden` = remove the image from the DOM on mobile screens
           
           the class `md:block` = show the image on desktop screens
+
+          ...
           */}
           <Image
             src='/hero-desktop.png'
             width={1000}
             height={760}
             className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop version"
+          />
+          <Image
+            src='/hero-mobile.png'
+            width={560}
+            height={620}
+            className="md:hidden block"
             alt="Screenshots of the dashboard project showing desktop version"
           />
         </div>
