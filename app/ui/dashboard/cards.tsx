949b13b68f98717eafdae1e3b1ffa7c8f8a69b69
Kaloyan Marinov <happy-2025-04-06-14-33@protonmail.com>
2026-05-19 22:58:40 +0200

    To create more of a staggered effect, you can group the cards using a wrapper component.

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -5,6 +5,7 @@ import {
   InboxIcon,
 } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
+import { fetchCardData } from '@/app/lib/data';
 
 const iconMap = {
   collected: BanknotesIcon,
@@ -13,23 +14,38 @@ const iconMap = {
   invoices: InboxIcon,
 };
 
+/*
+To create more of a _staggered_ effect,
+you can get multiple components to load in at the same time
+by using a wrapper component.
+*/
 export default async function CardWrapper() {
+  const {
+    totalPaidInvoices,
+    totalPendingInvoices,
+    numberOfInvoices,
+    numberOfCustomers,
+  } = await fetchCardData();
+
   return (
     <>
-      {/* NOTE: Uncomment this code in Chapter 9 */}
-
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
+      <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
       <Card
         title="Total Customers"
         value={numberOfCustomers}
         type="customers"
-      /> */}
+      />
     </>
   );
 }
 
+/*
+You can fetch data for each individual instance of the following component,
+but this could lead to a _popping_ effect as the cards load in,
+which can be visually jarring/agitating for users of the web UI.
+*/
 export function Card({
   title,
   value,
