da3379ed0d46defff48462131d134e9de43cdfef
Kaloyan Marinov <happy-2025-04-06-14-33@protonmail.com>
2026-05-22 23:10:13 +0200

    flesh out `app/dashboard/invoices/[id]/edit/page.tsx`, keeping it static still

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,3 +1,26 @@
+import Form from '@/app/ui/invoices/edit-form';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import { fetchCustomers } from '@/app/lib/data';
+
 export default function Page() {
-  return <div>hello</div>;
+  const id = 'xyz-123';
+  
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          {
+            label: 'Invoices',
+            href: '/dashboard/invoices',
+          },
+          {
+            label: 'Edit Invoice',
+            href: `/dashboard/invoices/${id}/edit`,
+            active: true,
+          }
+        ]}
+      />
+      {/* <Form invoice={invoice} customers={customers} /> */}
+    </main>
+  );
 }
