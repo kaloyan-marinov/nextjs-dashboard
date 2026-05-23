f20e6487e6043f78dfbb8e55fda276e5f8ff0f62
Kaloyan Marinov <happy-2025-04-06-14-33@protonmail.com>
2026-05-23 15:04:04 +0200

    flesh out `app/dashboard/invoices/error.tsx`

--- app/dashboard/invoices/error.tsx
+++ app/dashboard/invoices/error.tsx
@@ -6,8 +6,33 @@ and allows you to display a fallback UI to your users.
 */
 'use client';
 
-export default function Error() {
+import { useEffect } from 'react';
+
+export default function Error(props: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  const { error, reset } = props;
+  // `reset` is a function to reset the error boundary.
+  // When executed, the function will try to re-render the route segment.
+
+  useEffect(() => {
+    // Optionally log the error to an error-reporting service
+    console.error(error);
+  }, [error]);
+
   return (
-    <div>[error]</div>
+    <main className="flex h-full flex-col items-center justify-center">
+      <h2 className="text-center">Something went wrong!</h2>
+      <button
+        className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400"
+        onClick={
+            // Attempt to recover by trying to re-render the invoices route
+            () => reset()
+        }
+      >
+        Try again
+      </button>
+    </main>
   );
 }
