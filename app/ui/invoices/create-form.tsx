b32a10dcca373e7871b8c9f2102437ed36b0ac6e
Kaloyan Marinov <happy-2025-04-06-14-33@protonmail.com>
2026-05-24 00:26:12 +0200

    ensure that the last red line from the screenshot at https://nextjs.org/learn/dashboard-app/improving-accessibility#practice-adding-aria-labels will also get rendered

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -136,6 +136,11 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
             ))
           }
         </div>
+        {state.message &&
+          <p className="mt-2 text-sm text-red-500">
+            {state.message}
+          </p>
+        }
       </div>
       <div className="mt-6 flex justify-end gap-4">
         <Link
