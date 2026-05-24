c04aa538b287713438788edee730199144216748
Kaloyan Marinov <happy-2025-04-06-14-33@protonmail.com>
2026-05-24 13:53:37 +0200

    [step 1] augmenting the `Credentials` provider = utilize `zod` to validate the email and password before checking if the user exists in the database (similarly to how it was utilized in `app/lib/actions.ts`)

--- auth.ts
+++ auth.ts
@@ -12,8 +12,17 @@ because `bcrypt` relies on Node.js APIs not available in Next.js Proxy.
 import NextAuth from 'next-auth';
 import { authConfig } from './auth.config';
 import Credentials from 'next-auth/providers/credentials';
+import { z } from 'zod';
 
 export const { auth, signIn, signOut } = NextAuth({
   ...authConfig,
-  providers: [Credentials({})],
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const parsedCredentials = z
+          .object({ email: z.string().email(), password: z.string().min(6) })
+          .safeParse(credentials);
+      },
+    }),
+  ],
 });
