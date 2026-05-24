345ef2721fa3691c9530496a4580cc9f9a0ba10f
Kaloyan Marinov <happy-2025-04-06-14-33@protonmail.com>
2026-05-24 11:55:57 +0200

    augment `proxy.ts` by highlighting the advantage of employing [Next.js Proxy](https://nextjs.org/docs/app/api-reference/file-conventions/proxy)

--- proxy.ts
+++ proxy.ts
@@ -1,8 +1,16 @@
 import NextAuth from 'next-auth';
 import { authConfig } from './auth.config';
 
+// Initialize NextAuth.js and exporting its indicated property.
 export default NextAuth(authConfig).auth;
 
+/*
+Specify that it should run on specific paths.
+
+The advantage of employing Proxy for this task is that
+the protected routes will not even start rendering until the Proxy verifies the authentication,
+enhancing both the security and performance of your application.
+*/
 export const config = {
   // https://nextjs.org/docs/app/api-reference/file-conventions/proxy#matcher
   matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
