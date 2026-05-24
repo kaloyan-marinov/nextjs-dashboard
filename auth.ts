0aa32d7dcd669b8cf8e9898b70e1308b3a2efc05
Kaloyan Marinov <happy-2025-04-06-14-33@protonmail.com>
2026-05-24 12:22:41 +0200

    augment `auth.ts` by specifying the `Credentials` provider (from Next.js), which allows users to log in with a username and password
    There are other ... providers such as OAuth or email. See the [NextAuth.js docs](
      https://authjs.dev/getting-started/providers
    ) for a full list of options.

--- auth.ts
+++ auth.ts
@@ -11,7 +11,9 @@ because `bcrypt` relies on Node.js APIs not available in Next.js Proxy.
 
 import NextAuth from 'next-auth';
 import { authConfig } from './auth.config';
+import Credentials from 'next-auth/providers/credentials';
 
 export const { auth, signIn, signOut } = NextAuth({
   ...authConfig,
+  providers: [Credentials({})],
 });
