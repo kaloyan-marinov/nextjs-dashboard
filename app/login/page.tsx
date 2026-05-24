bc15ba9156ca899f04760638aed4614baa21a60c
Kaloyan Marinov <happy-2025-04-06-14-33@protonmail.com>
2026-05-24 10:40:56 +0200

    make `app/login/page.tsx` render `<LoginForm />` within a `<Suspense>` component`, b/c the form will (a little later) be made to access information from the incoming request (URL search params)

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,4 +1,6 @@
 import AcmeLogo from '@/app/ui/acme-logo';
+import LoginForm from '@/app/ui/login-form';
+import { Suspense } from 'react';
 
 export default function LoginPage() {
   return (
@@ -9,6 +11,9 @@ export default function LoginPage() {
             <AcmeLogo />
           </div>
         </div>
+        <Suspense>
+          <LoginForm />
+        </Suspense>
       </div>
     </main>
   );
