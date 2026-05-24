d5d253d8109c37a717679e0249540fada37685ae
Kaloyan Marinov <happy-2025-04-06-14-33@protonmail.com>
2026-05-24 12:13:51 +0200

    Be advised that, in a subsequent commit, `bcrypt` will be used to compare each user-provided password and the user's hashed one within the DB; as preparation for that and due to the fact that `bcrypt` relies on Node.js APIs not available in Next.js Proxy, this commit adds `auth.ts` (that spreads the `authConfig object)

---
+++ auth.ts
@@ -0,0 +1,17 @@
+/*
+[This project's mechanism for] seeding [the] database
+[utilizes] a package called `bcrypt` to hash the user's password before storing it in the database.
+
+[That same package will be used to verify] that
+the password entered by the user matches the one in the database.
+
+[This] separate file for the `bcrypt` package [needs to be created,]
+because `bcrypt` relies on Node.js APIs not available in Next.js Proxy.
+*/
+
+import NextAuth from 'next-auth';
+import { authConfig } from './auth.config';
+
+export const { auth, signIn, signOut } = NextAuth({
+  ...authConfig,
+});
