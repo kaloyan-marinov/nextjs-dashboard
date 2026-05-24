71001bcaf68fe4da25c1dff3538c7cb6f3ec2534
Kaloyan Marinov <happy-2025-04-06-14-33@protonmail.com>
2026-05-24 11:23:58 +0200

    add `auth.config.js`, which causes TypeScript to complain with ```shell Type '{ pages: { signIn: string; }; }' does not satisfy the expected type 'NextAuthConfig'. Property 'providers' is missing in type '{ pages: { signIn: string; }; }' but required in type 'NextAuthConfig'.ts(1360) ```

---
+++ auth.config.ts
@@ -0,0 +1,14 @@
+import type { NextAuthConfig } from 'next-auth';
+
+/*
+You can use the `pages` option to specify the route for custom sign-in, sign-out, and error pages.
+
+This is not required,
+but [the next statement causes] the user [to] be redirected to our custom login page,
+rather than the NextAuth.js default page.
+*/
+export const authConfig = {
+  pages: {
+    signIn: '/login',
+  },
+} satisfies NextAuthConfig;
